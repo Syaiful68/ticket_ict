98d4d7e6b599df46abcdc1a17df54a7ae8f988e8
Syaiful68 <syaifulSheva@gmail.com>
2025-07-17 15:10:57 +0700

    Create Ticket modul

---
+++ src/components/ListMenu.vue
@@ -0,0 +1,21 @@
+<script setup>
+//
+const props = defineProps({
+  label: String,
+  icon: String,
+  path: String,
+});
+</script>
+
+<template>
+  <li class="nav-item">
+    <router-link class="nav-link active" :to="path">
+      <div
+        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center"
+      >
+        <i class="fa-solid" :class="icon"></i>
+      </div>
+      <span class="nav-link-text ms-1">{{ label }}</span>
+    </router-link>
+  </li>
+</template>
---
+++ src/views/Layout/SideNav.vue
@@ -0,0 +1,42 @@
+<script setup>
+import List from "../../components/ListMenu.vue";
+const Menus = [
+  { label: "Dashboard", icons: "fa-gauge-high", path: "/dashboard" },
+  { label: "Tickets", icons: "fa-pen-to-square", path: "/ticket" },
+];
+</script>
+
+<template>
+  <aside
+    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3"
+    id="sidenav-main"
+  >
+    <div class="sidenav-header">
+      <i
+        class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+        aria-hidden="true"
+        id="iconSidenav"
+      ></i>
+      <a
+        class="navbar-brand m-0"
+        href=" https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html "
+        target="_blank"
+      >
+        <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo" />
+        <span class="ms-1 font-weight-bold">Dashboard</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0" />
+    <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <List
+          v-for="(item, index) in Menus"
+          :key="index"
+          :label="item.label"
+          :icon="item.icons"
+          :path="item.path"
+        ></List>
+      </ul>
+    </div>
+  </aside>
+</template>
