fd09e97fc5c24507f53995d945318efd5b441703
Syaiful68 <syaifulSheva@gmail.com>
2025-07-18 11:38:08 +0700

    change admin dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
 //
-import NavBar from "./views/Layout/NavBar.vue";
 import SideNav from "./views/Layout/SideNav.vue";
+import NavBar from "./views/Layout/NavBar.vue";
 </script>
 
 <template>
-  <SideNav v-if="$route.path !== '/login'"></SideNav>
-  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
-    <!-- Navbar -->
-    <NavBar v-if="$route.path !== '/login'"></NavBar>
-    <!-- End Navbar -->
-    <router-view></router-view>
-  </main>
+  <!--  -->
+  <div class="page">
+    <SideNav></SideNav>
+    <div class="page-wrapper">
+      <NavBar></NavBar>
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
--- src/views/Layout/NavBar.vue
+++ src/views/Layout/NavBar.vue
@@ -1,41 +1,53 @@
 <template>
-  <nav
-    class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl"
-    id="navbarBlur"
-    navbar-scroll="true"
-  >
-    <div class="container-fluid py-1 px-3">
-      <nav aria-label="breadcrumb">
-        <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-          <li class="breadcrumb-item text-sm">
-            <a class="opacity-5 text-dark" href="javascript:;">Pages</a>
-          </li>
-          <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
-        </ol>
-        <h6 class="font-weight-bolder mb-0">Dashboard</h6>
-      </nav>
-      <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-        <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+  <header class="navbar navbar-expand-md d-none d-lg-flex d-print-none">
+    <div class="container-xl">
+      <!-- BEGIN NAVBAR TOGGLER -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar-menu"
+        aria-controls="navbar-menu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- END NAVBAR TOGGLER -->
+      <div class="navbar-nav flex-row order-md-last">
+        <div class="d-none d-md-flex">
           <!--  -->
         </div>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item d-flex align-items-center">
-            <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-              <i class="fa fa-user me-sm-1"></i>
-              <span class="d-sm-inline d-none">Sign In</span>
-            </a>
-          </li>
-          <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
-            <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-              <div class="sidenav-toggler-inner">
-                <i class="sidenav-toggler-line"></i>
-                <i class="sidenav-toggler-line"></i>
-                <i class="sidenav-toggler-line"></i>
-              </div>
-            </a>
-          </li>
-        </ul>
+        <div class="nav-item dropdown">
+          <a
+            href="#"
+            class="nav-link d-flex lh-1 p-0 px-2"
+            data-bs-toggle="dropdown"
+            aria-label="Open user menu"
+          >
+            <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)">
+            </span>
+            <div class="d-none d-xl-block ps-2">
+              <div>Paweł Kuna</div>
+              <div class="mt-1 small text-secondary">UI Designer</div>
+            </div>
+          </a>
+          <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+            <a href="#" class="dropdown-item">Status</a>
+            <a href="./profile.html" class="dropdown-item">Profile</a>
+            <a href="#" class="dropdown-item">Feedback</a>
+            <div class="dropdown-divider"></div>
+            <a href="./settings.html" class="dropdown-item">Settings</a>
+            <a href="./sign-in.html" class="dropdown-item">Logout</a>
+          </div>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse" id="navbar-menu">
+        <!-- BEGIN NAVBAR MENU -->
+
+        <!-- END NAVBAR MENU -->
       </div>
     </div>
-  </nav>
+  </header>
+  <!-- END NAVBAR  -->
 </template>
